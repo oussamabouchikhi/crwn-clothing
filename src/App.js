04a775d8dc830a52071512d75a91924c5372a1a4
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-06 23:45:14 +0100

    Route Homepage & Hatspage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/Homepage';
 
+const Hatspage = () => <h1>HATS Page</h1>;
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route path='/' component={Homepage}></Route>
+        <Route path='/hats' component={Hatspage}></Route>
+      </Switch>
     </div>
   );
 }
