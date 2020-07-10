9221099ea23c054e49bd6d3bd552f073257d90d0
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-10 12:03:11 +0100

    Create CollectionPreview component

---
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './collection-preview.scss';
+
+const CollectionPreview = ({ title, items }) => {
+    return (
+        <div className="collection-preview">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <div className="preview">
+            {
+                items
+                    .filter((item, index) => index < 4) // show 4 items
+                    .map((item) =>  <div key={item.id}>{item.name}</div>)
+            }
+            </div>
+        </div>
+    )
+}
+
+export default CollectionPreview;
