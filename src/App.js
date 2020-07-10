c1fb8af071523a6c4f0d7334c2978821af619ecb
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-10 12:03:30 +0100

    Add shop data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/Homepage';
-
-const Hatspage = () => <h1>HATS Page</h1>;
-const TopicsList = () => <h1>Topics List Page</h1>;
-const TopicDetail = () => <h1>Topic Detail Page</h1>;
+import Shop from './pages/shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Homepage}></Route>
-        <Route path='/topics' component={TopicsList}></Route>
-        <Route path='/topics/:tpicId' component={TopicDetail}></Route>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route path='/shop' component={Shop}></Route>
       </Switch>
     </div>
   );
