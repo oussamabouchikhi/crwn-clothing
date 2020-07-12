9e282fb11d79c0c14a732c9e99201266076e19b2
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-12 11:37:30 +0100

    Create store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
