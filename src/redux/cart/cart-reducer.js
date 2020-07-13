cddf48927abc54a4735176b93d164355f9a67684
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-13 14:33:21 +0100

    Group added CartItems by quantity

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,4 +1,5 @@
 import types from './cart-types';
+import {addItemToCart} from './cart-utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case types.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             };
         default:
             return state;
