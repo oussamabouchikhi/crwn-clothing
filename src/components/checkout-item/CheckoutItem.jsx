de41183c31f4e34915d32e443f4851f96dfa7962
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-14 15:57:57 +0100

    Implement removeItem on decrease in Checkout page

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
-import {clearItemFromCart} from '../../redux/cart/cart-actions';
+import {clearItemFromCart, removeItem, addItem} from '../../redux/cart/cart-actions';
 
 import './checkout-item.scss';
 
@@ -14,7 +13,11 @@ const CheckoutItem = ({cartItem, dispatch}) => {
             <img src={imageUrl} alt="item"/>
         </div>
         <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
+        <div className="quantity">
+            <span className="arrow" onClick={()=> dispatch(removeItem(cartItem))}>&#10094;</span>
+            <span className="value">{quantity}</span>
+            <span className="arrow" onClick={()=> dispatch(addItem(cartItem))}>&#10095;</span>
+        </div>
         <span className="price">{price}</span>
         <div className="remove-button" onClick={()=> dispatch(clearItemFromCart(cartItem))}>&#10005;</div>
     </div>)
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,5 +1,5 @@
 import types from './cart-types';
-import {addItemToCart, clearItemFromCart} from './cart-utils';
+import {addItemToCart, removeItemFromCart} from './cart-utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             };
+        case types.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+                
+            }
         case types.CLEAR_ITEM_FROM_CART:
             return {
                 ...state,
