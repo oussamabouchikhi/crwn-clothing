eaa91046b2432f83515c1fbfd72c6d4849ede0ed
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-14 16:37:15 +0100

    Cache cart in LocalStorage using redux-persist

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import {BrowserRouter} from 'react-router-dom';
 
 import {Provider} from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
